feed32f9f2341b7808576da14259a79bb7609bdd
Ali Eray <alieraay@gmail.com>
2024-03-13 18:59:06 +0300

    feat(Square): Add update method for squaring numbers

---
+++ src/Square.ts
@@ -0,0 +1,17 @@
+import { state, State, Mina, method, Field, SmartContract } from 'o1js';
+
+export class Square extends SmartContract {
+  @state(Field) num = State<Field>();
+
+  init() {
+    super.init();
+    this.num.set(Field(3));
+  }
+
+  @method update(square: Field) {
+    const currentState = this.num.get();
+    this.num.requireEquals(currentState);
+    square.assertEquals(currentState.mul(currentState));
+    this.num.set(square);
+  }
+}
